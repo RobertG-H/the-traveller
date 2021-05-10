120ba8de0a253d818b2afe9c903b634a7ba0a714
Robert Graham-Hu <robertgrahamhu@gmail.com>
2021-05-10 16:12:41 -0400

    Added raycast based collision detection. Doesn't handle angled walls

--- the-traveller-unity/Assets/Player/PlayerController.cs
+++ the-traveller-unity/Assets/Player/PlayerController.cs
@@ -10,22 +10,30 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;
 
+    PlayerPhysics physics;
+
     void Awake()
     {
         animator = GetComponent<Animator>();
+        physics = GetComponent<PlayerPhysics>();
+    }
+
+    void Update()
+    {
+        //todo move this to state
+        physics.Walk(new Vector2(iHorz, iVert), Time.deltaTime);
+        animator.SetFloat("Speed", new Vector2(iHorz, iVert).magnitude);
+
     }
 
     public void OnHorizontal(InputAction.CallbackContext context)
     {
         iHorz = context.ReadValue<float>();
         animator.SetFloat("Horizontal", iHorz);
-        animator.SetFloat("Speed", Mathf.Abs(iHorz));
     }
     public void OnVertical(InputAction.CallbackContext context)
     {
         iVert = context.ReadValue<float>();
         animator.SetFloat("Vertical", iVert);
-        animator.SetFloat("Speed", Mathf.Abs(iVert));
-
     }
 }
--- the-traveller-unity/Assets/Player/PlayerPhysics.cs
+++ the-traveller-unity/Assets/Player/PlayerPhysics.cs
@@ -1,25 +1,232 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public static class LayerMaskConfig
+{
+    public static int DEFAULT;
+    public static void initConfig()
+    {
+        //Layers that the player should ignore
+        DEFAULT = 1 << LayerMask.NameToLayer("Player");
+
+        DEFAULT = ~DEFAULT;
+    }
+}
 
 public class PlayerPhysics : MonoBehaviour
 {
-    [SerializeField]
-    PlayerController p;
-    [SerializeField]
-    private float speed;
-    private int decimals = 4;
-    void FixedUpdate()
+    [SerializeField] PlayerController p;
+    [SerializeField] BoxCollider2D ECB;
+
+    #region Local State Variables
+    Vector2 velocity = Vector2.zero;
+    // Vector2 facingDirection = Vector2.right;
+
+    #endregion
+
+
+    #region Physics Constants
+    [Header("Physics Constants")]
+    [SerializeField] float WALK_MAX_SPEED;
+    [SerializeField] float WALK_ACCEL;
+    [SerializeField] float FRICTION_GROUND;
+
+    #endregion
+
+    #region Raycasting variables
+    [Header("Raycasting")]
+    [SerializeField] float SKIN_WIDTH;
+    [SerializeField] int HORIZONTAL_RAY_COUNT;
+    [SerializeField] int VERTICAL_RAY_COUNT;
+    public RaycastOrigins raycastOrigins;
+    private float horizontalRaySpacing;
+    private float verticalRaySpacing;
+    public struct RaycastOrigins
     {
-        Vector2 velocity = new Vector2(p.iHorz, p.iVert);
-        ApplyMovement(velocity * Time.fixedDeltaTime * speed);
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+    #endregion
+
+    #region Debug
+    [Header("Debug")]
+    [SerializeField] bool isDebug = false;
+
+    #endregion
+
+    private int decimals = 4;
+
+    void Awake()
+    {
+        LayerMaskConfig.initConfig();
     }
 
-    private void ApplyMovement(Vector3 displacement)
+    void Start()
     {
-        gameObject.transform.Translate(new Vector3((float)System.Math.Round(displacement.x, decimals),
-        (float)System.Math.Round(displacement.y, decimals),
-        (float)System.Math.Round(displacement.z, decimals)),
-        relativeTo: Space.World);
+        CalculateRaySpacing();
     }
+
+    void OnGUI()
+    {
+        if (!isDebug) return;
+        GUI.Label(new Rect(10, 10, 100, 20), string.Format("Velocity: {0}", velocity.x));
+    }
+
+    void FixedUpdate()
+    {
+
+        ApplyFriction();
+        Vector2 displacement = velocity * Time.fixedDeltaTime;
+        ApplyMovement(displacement);
+    }
+
+
+    #region Inherent movement method 
+    void ApplyFriction()
+    {
+        if (velocity.magnitude == 0) return;
+
+        Vector2 velocityDeltaFromFriction = Time.fixedDeltaTime * velocity.normalized * -1;
+
+        velocityDeltaFromFriction *= FRICTION_GROUND;
+
+        // Ensure friction never adds so much negative force it moves the palyer backwards
+        if (velocityDeltaFromFriction.magnitude > velocity.magnitude)
+        {
+            velocityDeltaFromFriction = velocity * -1;
+        }
+
+        AddVel(velocityDeltaFromFriction);
+    }
+
+    private void ApplyMovement(Vector2 displacement)
+    {
+        UpdateRaycastOrigins();
+        HorizontalCollisions(ref displacement);
+        VerticalCollisions(ref displacement);
+        gameObject.transform.Translate(new Vector3((float)System.Math.Round(displacement.x, decimals), (float)System.Math.Round(displacement.y, decimals), 0));
+    }
+    #endregion
+
+    #region Public movement methods
+    public void Walk(Vector2 input, float deltaTime)
+    {
+        AddVel(WALK_ACCEL * input.normalized * deltaTime);
+        if (Mathf.Abs(velocity.magnitude) > WALK_MAX_SPEED)
+        {
+            SetVel(WALK_MAX_SPEED * input.normalized);
+        }
+    }
+    #endregion
+
+    #region Raycasting Methods
+    void HorizontalCollisions(ref Vector2 displacement)
+    {
+        float rayLength = Mathf.Abs(displacement.x) + SKIN_WIDTH;
+        float directionX = Mathf.Sign(displacement.x);
+
+        for (int i = 0; i < HORIZONTAL_RAY_COUNT; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, LayerMaskConfig.DEFAULT);
+            if (hit)
+            {
+                displacement.x = (hit.distance == 0) ? 0 : (hit.distance - SKIN_WIDTH) * directionX;
+                rayLength = hit.distance;
+                SetVel(x: 0);
+                Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.yellow);
+            }
+        }
+    }
+    void VerticalCollisions(ref Vector2 displacement)
+    {
+        float rayLength = Mathf.Abs(displacement.y) + SKIN_WIDTH;
+        float directionY = Mathf.Sign(displacement.y);
+
+        for (int i = 0; i < HORIZONTAL_RAY_COUNT; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + displacement.x);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, LayerMaskConfig.DEFAULT);
+
+            if (hit)
+            {
+                displacement.y = (hit.distance == 0) ? 0 : (hit.distance - SKIN_WIDTH) * directionY;
+                rayLength = hit.distance;
+                SetVel(y: 0);
+                Debug.DrawRay(rayOrigin, Vector2.right * directionY * rayLength, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(rayOrigin, Vector2.right * directionY * rayLength, Color.yellow);
+            }
+        }
+    }
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = ECB.bounds;
+        bounds.Expand(SKIN_WIDTH * -2);
+
+        HORIZONTAL_RAY_COUNT = Mathf.Clamp(HORIZONTAL_RAY_COUNT, 2, int.MaxValue);
+        VERTICAL_RAY_COUNT = Mathf.Clamp(VERTICAL_RAY_COUNT, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (HORIZONTAL_RAY_COUNT - 1);
+        verticalRaySpacing = bounds.size.x / (VERTICAL_RAY_COUNT - 1);
+    }
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = ECB.bounds;
+        bounds.Expand(SKIN_WIDTH * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+
+    #endregion
+
+    #region Getters Setters and Public Info Methods
+    public void AddVel(float x = 0, float y = 0)
+    {
+        velocity = new Vector2(velocity.x + x, velocity.y + y);
+    }
+    public void AddVel(Vector2 vel)
+    {
+        velocity += vel;
+    }
+    public void SetVel(float? x = null, float? y = null)
+    {
+        if (x != null && y != null)
+        {
+            velocity = new Vector2(x.Value, y.Value);
+        }
+
+        else if (x != null && y == null)
+        {
+            velocity = new Vector2(x.Value, velocity.y);
+        }
+        else if (y != null && x == null)
+        {
+            velocity = new Vector2(velocity.x, y.Value);
+        }
+    }
+    public void SetVel(Vector2 vel)
+    {
+        velocity = vel;
+    }
+
+    public Vector2 GetVel()
+    {
+        return velocity;
+    }
+    #endregion
 }
